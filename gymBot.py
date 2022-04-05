19e62eaecd4838f9f5365997e406f5adcd9b895a
Lucaster13 <terr@usc.edu>
2022-04-04 18:36:39 -0700

    idk

--- gymBot.py
+++ gymBot.py
@@ -1,5 +1,7 @@
+from scheduleUtils import createActiveBooking, createSched, isCompatible, modifySched, _DOW, getSavedScheduleFromFile
 import time
 import schedule
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -14,65 +16,92 @@ from selenium.webdriver.support import expected_conditions as EC
 
 _USC_USER = "terr"
 _USC_PASS = "13jan199913jan1999"
-_BOOKING_MONTH = "9"
-_BOOKING_DAY = "24"
+
+def main():
+
+    savedSched = getSavedScheduleFromFile()
+
+    # ask if user wants to modify saved sched
+    if not savedSched["empty"]:
+        if input("\nYou currently have a schedule saved, would you like to modify it? [y/n]: ") == "y":
+            modifySched()
+        else:
+            print("\nrunning GymBot...")
+    else:
+        print("\nWARNING: You do not currently have a schedule saved, you must create one.");
+        createSched()
+
+    # do not let user progress with empty schedule
+    while True:
+        sched = getSavedScheduleFromFile()
+        if sched["empty"]:
+            print("\nWARNING: Your schedule is currently empty, you must add an availability to continue (or ctrl + c to exit)")
+            createSched()
+        else:
+            break
+
+    # open browser window
+    _GYM_URL = "https://myrecsports.usc.edu/booking"
+    browser = webdriver.Chrome()
+    browser.get(_GYM_URL)
+    browser.maximize_window()
+    browser.execute_script("{showLogin('/booking');submitExternalLoginForm('Shibboleth');}")
+
+    while True:
+        sched = getSavedScheduleFromFile()
+
+        for dow in _DOW:
+            try:
+                # wait for usc shibboleth button to show up
+                shibbolethBtn = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='divLoginOptions']/div[2]/div[2]/div/button")))
+                shibbolethBtn.click()
+
+                # find user and pass input and login
+                userInput = browser.find_element_by_id("username")
+                passInput = browser.find_element_by_id("password")
+
+                userInput.send_keys(_USC_USER)
+                passInput.send_keys(_USC_PASS, Keys.ENTER)
+
+                print(f"{_USC_USER} successfully logged in thru Shibboleth")
+
+                # wait for usc login and then fitness button
+                villageBookingsBtn = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[@data-product-id='cd93ade2-af9d-4e5f-84e0-06e10711b5ce']")))
+                villageBookingsBtn.click()
+
+                # find bbooking day button and select correct day
+                bookingDayBtn = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, f"//*[@id='divBookingDateSelector']/div[2]//button[span[text()='Sat']]")))
+                bookingDayBtn.click()
+
+                # Find bookable times and locate book buttons
+                bookingButtons = WebDriverWait(browser, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//button[@data-timeslot-id]")))
+
+                # Loop over buttons and check if times are compatible with chosen times
+                for button in bookingButtons:
+                    bookingTime = button.get_attribute("data-slot-text")
+                    print(f"Opening found from {bookingTime}, checking compatibility...")
+                    if isCompatible(bookingTime, dow):
+                        print("Booking time is compatible with schedule, securing booking now!")
+                        makeBooking(button, bookingTime, dow)
+
+            except Exception as e:
+                print(e)
+            finally:
+                browser.close()
+        
+        # sleep for 5 mins
+        time.sleep(300)
 
 
-# open browser window
-_GYM_URL = "https://myrecsports.usc.edu/booking"
-browser = webdriver.Chrome()
-browser.get(_GYM_URL)
-browser.maximize_window()
-browser.execute_script(
-    "{showLogin('/booking');submitExternalLoginForm('Shibboleth');}")
-
-# create action obj
-action = ActionChains(browser)
-
-with open('schedule.json', 'w+') as savedSched:
-        if not savedSched.empty:
-            print("You currently have a schedule saved, would you like to overwrite it?");
-            input("y/n: ")
-            
-        sched = savedSched;
-        json.dump(, f)
+def makeBooking(bookingButton, bookingTime, dow):
+    try:  
+        bookingButton.click()
+        createActiveBooking(bookingTime, dow)
+    except Exception:
+        print(f"Something went wrong when trying to book {bookingTime}")
 
 
-def runScheduler():
-
-    try:
-        # wait for usc shibboleth button to show up
-        shibbolethBtn = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='divLoginOptions']/div[2]/div[2]/div/button")))
-        shibbolethBtn.click()
-
-        # find user and pass input and login
-        userInput = browser.find_element_by_id("username")
-        passInput = browser.find_element_by_id("password")
-
-        userInput.send_keys(_USC_USER)
-        passInput.send_keys(_USC_PASS, Keys.ENTER)
-
-        print(f"{_USC_USER} successfully logged in thru Shibboleth")
-
-        # wait for usc login and then fitness button
-        villageBookingsBtn = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[@data-product-id='cd93ade2-af9d-4e5f-84e0-06e10711b5ce']")))
-        villageBookingsBtn.click()
-
-        # find bbooking day button and select correct day
-        bookingDayBtn = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, f"//*[@id='divBookingDateSelector']/div[2]//button[@data-day='{_BOOKING_DAY}']")))
-        bookingDayBtn.click()
-
-        # Find bookable times and locate book buttons
-        bookingButtons = WebDriverWait(browser, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//button[@data-timeslot-id]")))
-
-        # Loop over buttons and check if times are compatible with chosen times
-        for button in bookingButtons:
-            bookingTime = button.get_attribute("data-slot-text")
-            print(f"Opening found from {bookingTime}, checking compatibility...")
-
-    except Exception as e:
-        print(e)
-    finally:
-        browser.close()
 
 
+# RUN SCRIPT
+main()
--- scheduleUtils.py
+++ scheduleUtils.py
@@ -1,111 +1,435 @@
 import json
-from icalendar import Calendar
-import fileinput
+import traceback
+from time import daylight
+
+_SAVED_SCHEDULE_FILE = 'schedule.json'
+_DOW = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"]
 
 
-_DOW = ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"]
 
 
-def checkSched(sched): 
+def modifySched(): 
     while True:
 
-        print("To choose a day, enter its number/letter")
+        sched = getSavedScheduleFromFile()
+
+        print("To choose a day, enter the value in the brackets.\n")
         for idx, d in enumerate(_DOW):
             print(f"[{idx}]: {d}")
+        if not sched["empty"]:
+            print("[clear]: CLEAR SCHEDULE")
         print("[q]: QUIT")
+        
         exit = False
         
         while True:
-            dayIdx = input("\n:")
-    
-            if dayIdx == "q":
-                exit = True
-                break
-
+            option = input("\n: ")
             try:
-                dayIdx = int(dayIdx)
-                if dayIdx >= 0 and day < 7:
-                    showDay(sched, dayIdx)
-                    if not input("Enter 'y' to edit new day OR any other key to quit: ") == "y":
-                        break
+                if option == "q":
+                    exit = True
+                    break
+                elif option == "clear":
+                    if input("Are you sure you want to clear your schedule? [y/n]: ") == "y":
+                        clearSched()
+                        createSched()
                 else:
-                    raise Exception()
-            except Exception:
-                print(f"Invalid choice: '{dayIdx}', please try again")
-
+                    option = int(option)
+                    if option >= 0 and option < 7:
+                        modifyDay(_DOW[option])
+                        print("[y]: edit different day")
+                        print("[q]: quit")
+                        if not input("\n: ") == "y":
+                            break
+                    else:
+                        print(f"\nInvalid choice: '{option}', please try again")
+            except Exception as e:
+                print("\n\n", traceback.print_exc(), "\n\n")
+                    
         if exit:
             break
 
-def showDay(sched, dayIdx): 
-    day = sched[dayIdx]
+def modifyDay(dow):
+    sched = getSavedScheduleFromFile()
+    day = sched[dow]
+    isEmptyDay = len(day["availabilities"]) > 0
 
-    print(f"--- {day.name} ---\n")
-    print(json.dumps(day, indent=2))
+    print(f"\n--- {day['name']} ---")
+    printDay(day)
 
     print("What would you like to do:")
     print("[1]: add new availability")
-    print("[2]: delete availability")
-    if day.activeBooking is not None:
-        print("[3]: cancel active booking")
+    if isEmptyDay:
+        print("[2]: delete availability")
+        print("[3]: modify existing availability")
+        print("[4]: clear availabilities")
+    if day["activeBooking"] is not None:
+        print("[6]: cancel active booking")
     print("[q]: QUIT")
 
     while True:
+        num = input("\n: ")
+    
+        if num == "q":
+            break
+
+        num = int(num)
+        if num >= 1 and num < 4:
+            {
+                1: addNewAvail,
+                2: deleteAvail,
+                4: modifyAvail,
+                5: clearAvails,
+                # 6: cancelActiveBooking
+            }[num](dow)
+        else:
+            print(f"\nInvalid choice: '{num}', please try again")
+
+def addNewAvail(dow):
+    sched = getSavedScheduleFromFile()
+    currAvails = sched[dow]["availabilities"]
+
+    while True: 
+        print("\n--- Current Availabilities ---")
+        printAvails(currAvails)
+        print("\n--- Add New Availability (Time Format hh:mm am/pm) ---\n")
+
+        startTimeFull = input("Start Time: ").strip();
+        endTimeFull = input("End Time: ").strip();
+        newAvailStr = f"{startTimeFull} - {endTimeFull}";
+        newValidAvail = checkIsValidAvail(newAvailStr)
+
+        if newValidAvail is not None:
+            print(f"\nAttempting to add new availability ({newValidAvail['fullTime']})...")
+            # check for overlaps
+            overlaps = findOverlaps(newValidAvail, currAvails)
+            if len(overlaps) == 0:
+                # add directly
+                sched[dow]['availabilities'].append(newValidAvail)
+            else:
+                print(f"New availability {newValidAvail} overlaps with {overlaps}");
+                if input("Would you like to merge these? [y/n]: ") == "y":
+                    mergeAvails(newValidAvail, overlaps, dow)
+            
+            # overwrite saved sched
+            print(f"Successfully added new availability {newValidAvail['fullTime']}!")
+            saveScheduleToFile(sched)
+            print(f"Availabilities for {sched[dow]['name']}:")
+            printAvails(sched[dow]["availabilities"])
+
+        if input("\nEnter q to quit, or any other key to add another availability: ") == "q":
+                break
+
+def deleteAvail(dow):
+    sched = getSavedScheduleFromFile()
+    currAvails = sched[dow]["availabilities"]
+    print("Which availability would you like to delete:")
+
+    while True:
+        
+        for idx, avail in enumerate(currAvails):
+            print(f"[{idx}]: {avail['fullTime']}")
+        print("[q]: QUIT")
+
         num = input("\n:")
     
         if num == "q":
             break
 
-        try:
-            num = int(num)
-            if num >= 1 and num < 4:
-                {
-                    1: addNewAvail,
-                    2: deleteAvail,
-                    3: cancelActiveBooking
-                }[num](sched, dayIdx)
-            else:
-                raise Exception()
-        except Exception:
-            print(f"Invalid choice: '{num}', please try again")
+        num = int(num)
+        if num >= 0 and num < len(currAvails):
+            print(f"Attempting to delete availability ({currAvails[num]['fullTime']})...")
+            del sched[dow]["availabilities"][num]
+            saveScheduleToFile(sched)
+            print("[y]: delete another availability")
+            print("[q]: quit")
+            if not input("\n: ") == "y":
+                break
+        else:
+            print(f"\nInvalid choice: '{num}', please try again.")
 
-def addNewAvail(sched, dayIdx):
-    currAvails = sched[dayIdx].availabilities
-    print(f"Current Availabilities: {currAvails}")
-    print("Add New Availability:")
-    print("Times must be in the form hh:mm am/pm\n")
-    startTimeFull = input("Start Time: ");
-    endTimeFull = input("End Time: ");
-    overlaps = checkIsValidAvail(startTimeFull, endTimeFull, currAvails);
+        
 
-    if overlaps == 0:
-        # add directly
-        sched[dayIdx].availabilites.push(f"{startTimeFull} - {endTimeFull}")
-    if overlaps.len > 0:
-        if input(f"Would you like to merge {overlaps} with {newAvail}"):
-            
+def modifyAvail(dow):
+    sched = getSavedScheduleFromFile()
+    currAvails = sched[dow]["availabilities"]
+    print("Which availability would you like to modify:")
 
+    while True:
 
-def checkIsValidAvail(newAvail, currAvails):
+        for idx, avail in enumerate(currAvails):
+            print(f"[{idx}]: {avail['fullTime']}")
+        print("[q]: QUIT")
+
+        num = input("\n: ")
+    
+        if num == "q":
+            break
+
+        num = int(num)
+        if num >= 0 and num < len(currAvails):
+            currAvail = currAvails[num]
+            print("Times must be in the form hh:mm am/pm\n")
+
+            startTimeFull = input(f"Start Time (currently {currAvail['fullTime'].split('-')[0].strip()}): ");
+            endTimeFull = input(f"End Time (currently {currAvail['fullTime'].split('-')[1].strip()}): ");
+
+            newAvailStr = f"{startTimeFull} - {endTimeFull}";
+
+            print(f"Attempting to change availability ({currAvails[num]['fullTime']}) to ({newAvailStr})...")
+
+            newValidAvail = checkIsValidAvail(newAvailStr)
+
+            if newValidAvail is not None:
+                overlaps = findOverlaps(newValidAvail, currAvails)
+                
+                # always one overlap, if more, prompt user
+                if len(overlaps) > 1:
+                    print(f"Modified availability {newValidAvail} now overlaps with {overlaps}");
+                    if input("Would you like to merge these? [y/n]: "):
+                        mergeAvails(newValidAvail, overlaps, dow)
+                else:
+                    mergeAvails(newValidAvail, overlaps, dow)
+
+            saveScheduleToFile(sched)
+            print("[y]: modify different availability")
+            print("[q]: quit")
+            if not input("\n: ") == "y":
+                break
+        else:
+            print(f"\nInvalid choice: '{num}', please try again.")
+        
+
+def clearAvails(dow):
+    sched = getSavedScheduleFromFile()
+    if input(f"Are you sure you want to clear availabilities for {sched[dow]['name']}? [y/n]: ")  == "y":
+        print(f"Clearing availabilities for {sched[dow]['name']}")
+        sched[dow]["availabilities"] = []
+        saveScheduleToFile(sched)
+
+# def cancelActiveBooking(sched, dayIdx):    
+
+def checkIsValidAvail(newAvailStr):
+    availObj = makeAvailObjFromStr(newAvailStr)
+
+    sHrs = availObj["startTime"]["hrs"]
+    sMins = availObj["startTime"]["mins"]
+    eHrs = availObj["endTime"]["hrs"]
+    eMins = availObj["endTime"]["mins"]
+
+    sArmy = availObj["startTime"]["army"]
+    eArmy = availObj["endTime"]["army"]
+
+    isValidHrs = sHrs >= 0 or sHrs <= 12 or eHrs >= 0 or eHrs <= 12
+    isValidMins = sMins >= 0 or sMins <= 59 or eMins >= 0 or eMins <= 59
+    isValidTime = eArmy >= sArmy
+
+    if not isValidHrs or not isValidMins or not isValidTime:
+        print(f"\nInvalid availability provided: {newAvailStr}, please check syntax and try again")     
+        return None
+    else:
+        return availObj
+        
+        
+
+def makeAvailObjFromStr(availStr):
+    # availStr takes the following form: hh:mm am/pm - hh:mm am/pm 
+
+    fullTimes = availStr.split("-")
+    startTime = fullTimes[0].strip();
+    endTime = fullTimes[1].strip();
+
+    startTimeObj = getTimeComponents(startTime)
+    endTimeObj = getTimeComponents(endTime)
+
+    sHrsFormat = startTimeObj["hrs"] if startTimeObj["hrs"] >= 10 else f"0{startTimeObj['hrs']}"
+    sMinsFormat = startTimeObj["mins"] if startTimeObj["mins"] >= 10 else f"0{startTimeObj['mins']}"
+    sAmPm = startTimeObj["amPm"]
+    eHrsFormat = endTimeObj["hrs"] if endTimeObj["hrs"] >= 10 else f"0{endTimeObj['hrs']}"
+    eMinsFormat = endTimeObj["mins"] if endTimeObj["mins"] >= 10 else f"0{endTimeObj['mins']}"
+    eAmPm = endTimeObj["amPm"]
+
+    fullTime = f"{sHrsFormat}:{sMinsFormat} {sAmPm} - {eHrsFormat}:{eMinsFormat} {eAmPm}"
+
+    return {
+        "fullTime": fullTime,
+        "startTime": startTimeObj,
+        "endTime": endTimeObj
+    }
+
+def getTimeComponents(timeStr):
+    # timeStr takes form ---> hh:mm am/pm
+    timeComponents = timeStr.split(" ");
+    time = timeComponents[0].strip().split(":")
+    amPm = timeComponents[1].strip()
+    hrs = int(time[0].strip())
+    mins = int(time[1].strip())
+    armyTime = getArmyTime(hrs, mins, amPm)
+        
+    return {
+        "amPm": amPm,
+        "hrs": hrs,
+        "mins": mins,
+        "army": armyTime
+    }
+
+def getArmyTime(hrs, mins, amPm):
+    minsFormatted = mins if mins > 10 else f"0{mins}"
+    hrsFormatted = ""
+    if amPm == "pm":
+        if hrs == 12:
+            hrsFormatted = hrs
+        else:
+            hrsFormatted = hrs + 12
+    elif amPm == "am":
+        if hrs == 12:
+            hrsFormatted = "00"
+        else:
+            hrsFormatted = hrs
+    else:
+        # invalid amPm
+        raise Exception()
+
+    return f"{hrsFormatted}:{minsFormatted}"
+
+def findOverlaps(newAvail, currAvails):
+    # filter all availabilities that overlap newAvail
+    return list(filter(lambda avail: isOverlap(newAvail, avail), currAvails))
+
+def isOverlap(avail1, avail2):
+    sArmy1 = avail1["startTime"]["army"]
+    sArmy2 = avail2["startTime"]["army"]
+
+    eArmy1 = avail1["endTime"]["army"]
+    eArmy2 = avail2["endTime"]["army"]
+
+    if sArmy1 < sArmy2:
+        return eArmy1 >= sArmy2
+    elif sArmy1 == sArmy2:
+        return True
+    else:
+        return sArmy1 <= eArmy2
+        
+def mergeAvails(newAvail, overlaps, dow):
+    sched = getSavedScheduleFromFile()
+
+    # get smallest start time and largest end time
+    # overlaps are sorted, compare newAvail w first and last overlap
+    startTime = {}
+    endTime = {}
+    currStartTime = overlaps[0]["startTime"]["army"]
+    currEndTime = overlaps[len(overlaps) - 1]["endTime"]["army"]
+
+    if newAvail["startTime"]["army"] < currStartTime:
+        startTime = newAvail["startTime"]
+    else:
+        startTime = overlaps[0]["startTime"]
+
+    if newAvail["endTime"]["army"] > currEndTime:
+        endTime = newAvail["endTime"]
+    else:
+        endTime = overlaps[len(overlaps) - 1]["endTime"]
+
+    mergedAvail = {
+        "fullTime": f"{startTime['hrs']}:{startTime['mins']} {startTime['amPm']} - {endTime['hrs']}:{endTime['mins']} {endTime['amPm']}",
+        "startTime": startTime,
+        "endTime": endTime
+    }
+
+    # remove overlaps from sched and add new merged avail
+    avails = sched[dow]["availabilities"]
+    modifiedAvails = list(filter(lambda avail: not isOverlap(avail, mergedAvail), avails))
+    modifiedAvails.append(mergedAvail)
+
+    # set schedule
+    sched[dow]["availabilities"] = modifiedAvails
+
+def saveScheduleToFile(sched):
+    
+    # check if empty
+    isEmpty = True
+    for attr, dayObj in sched.items():
+        if not attr == "empty" and len(dayObj["availabilities"]) > 0:
+            isEmpty = False
+            break
+    sched["empty"] = isEmpty
+
+    # sort all availabilities by start times
+    for attr, obj in sched.items():
+        if not attr == "empty":
+            sched[attr]["availabilities"].sort(key=lambda avail: avail["startTime"]["army"])
+
+    with open(_SAVED_SCHEDULE_FILE, 'w') as schedFile:
+        print(f"Saving schedule to {_SAVED_SCHEDULE_FILE}")
+        json.dump(sched, schedFile)
     
 
-
-def mergeAvails()
+def getSavedScheduleFromFile():
+    with open(_SAVED_SCHEDULE_FILE, 'r') as schedFile:
+        return json.load(schedFile)
     
-def deleteAvail(sched, dayIdx):
+def clearSched():
+    # write empty schedule to file
+    saveScheduleToFile({
+        "empty": True
+    })
 
-def cancelActiveBooking(sched, dayIdx):            
+def createSched():
+    sched = {
+        "empty": True
+    }
+    for dow in _DOW:
+        sched[dow] = {
+            "name": dow + "day",
+            "availabilities": [],
+            "activeBooking": None,
+        }
+    with open(_SAVED_SCHEDULE_FILE, 'w') as schedFile:
+        json.dump(sched, schedFile)
+    modifySched()
 
-def cleanSchedule(sched):
+def isCompatible(bookingTimeStr, dow):
+    day = getSavedScheduleFromFile()[dow]
+    spansNoon = "am" in bookingTimeStr.split("-")[0].strip()
 
+    bookingTimeObj = {}
 
-def makeSched():
-    # open json file to check data
+    # create the booking time obj
+    if spansNoon:
+        # bookingTime is valid format
+        bookingTimeObj = makeAvailObjFromStr(bookingTimeStr)
+    else:
+        # fix format of bookingTime
+        amPm = bookingTimeStr.split("-")[1].strip().split(" ")[1].strip()
+        bookingStart = f"{bookingTimeStr.split('-')[0].strip()} {amPm}"
+        bookingEnd = bookingTimeStr.split("-")[1].strip()
+        validBookingTimeStr = f"{bookingStart} - {bookingEnd}"
+        bookingTimeObj = makeAvailObjFromStr(validBookingTimeStr)
 
+    # find overlaps for bookingTime
+    overlaps = findOverlaps(bookingTimeObj, day["availabilities"])
 
-    icsFile = open(fileinput.input(input()), 'rb');
-    userSchedule = Calendar.from_ical(icsFile.read());
+    for overlap in overlaps:
+        bookingStart = bookingTimeObj["startTime"]["army"]
+        availStart = overlap["startTime"]["army"]
 
-    schedule = 
+        bookingEnd = bookingTimeObj["endTime"]["army"]
+        availEnd = overlap["endTime"]["army"]
 
-    input()
-    
+        # compatible booking found if this evaluates to true
+        return bookingStart >= availStart and bookingEnd <= availEnd
+      
+    return False
+
+def createActiveBooking(bookingTime, dow):
+    sched = getSavedScheduleFromFile()
+    print(f"Creating new active booking in schedule for {sched[dow]['name']} at {bookingTime}")
+    sched[dow].activeBooking = bookingTime
+    saveScheduleToFile(sched)
+
+def printAvails(avails):
+    print(json.dumps(list(map(lambda a: a["fullTime"], avails)), indent=2))
+
+def printDay(day):
+    avails = day['availabilities']
+    day['availabilities'] = list(map(lambda a: a["fullTime"], avails))
+    print(json.dumps(day, indent=2), "\n")
